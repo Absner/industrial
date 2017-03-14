2d92643f7381e344bf69cc342e73880fb9f48628
Absner <absrraniel@gmail.com>
2017-03-14 05:30:03 -0400

    sin errores

---
+++ src/simulacionindustrial/SimulationMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulacionindustrial;
+
+/**
+ *
+ * @author Raelyx-Yxzandra
+ */
+public class SimulationMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaSimulacion nuevo= new VentanaSimulacion();
+            nuevo.setVisible(true);
+    }
+    
+}
